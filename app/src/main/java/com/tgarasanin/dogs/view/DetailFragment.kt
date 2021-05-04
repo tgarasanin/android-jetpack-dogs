7718adf78cceaaa40ec2ecffa4afd6b08d3c3d40
Teodora Garasanin <teodora.garashanin@gmail.com>
2021-05-04 18:49:01 +0200

    feat: navigation

--- app/src/main/java/com/tgarasanin/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/tgarasanin/dogs/view/DetailFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import com.tgarasanin.dogs.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
+
+    private var dogUuid: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +26,18 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            textView3.text = dogUuid.toString()
+        }
+
+        buttonList.setOnClickListener {
+            val action: NavDirections = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
+
--- app/src/main/java/com/tgarasanin/dogs/view/ListFragment.kt
+++ app/src/main/java/com/tgarasanin/dogs/view/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.tgarasanin.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +19,18 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            action.dogUuid = 5
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 
 }
